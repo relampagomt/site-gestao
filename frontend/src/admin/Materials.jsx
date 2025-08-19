f9298c15ceccbb33340bbdb2f6f45543d216c48d
Beepy <gitbeepy@gmail.com>
2025-08-19 15:51:24 -0400

    Refatoração Geral - Padronização v051

--- frontend/src/admin/Materials.jsx
+++ frontend/src/admin/Materials.jsx
@@ -629,7 +629,8 @@ const Materials = () => {
 
           {/* Tabela */}
           <div className="overflow-x-auto rounded-xl border bg-card">
-            <Table>
+            {/* Reforço global: centraliza TODOS os th/td desta tabela */}
+            <Table className="[&_th]:!text-center [&_td]:!text-center">
               <TableHeader>
                 <TableRow>
                   <TableHead className="text-xs md:text-sm !text-center">Data</TableHead>
