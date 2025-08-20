c2c45549d6c31b5f20bbb59a68c17b31ff0ac13c
Beepy <gitbeepy@gmail.com>
2025-08-20 17:39:28 -0400

    Refatoração Geral - Padronização v114

--- frontend/src/admin/Finance.jsx
+++ frontend/src/admin/Finance.jsx
@@ -1,6 +1,7 @@
 // frontend/src/admin/Finance.jsx
 import React, { useEffect, useMemo, useState } from 'react';
 import api from '@/services/api';
+import { Upload, Search } from "lucide-react";
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card.jsx';
 import { Button } from '@/components/ui/button.jsx';
